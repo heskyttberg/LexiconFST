2ad682b99d21dd9ce014b81350b3ccf97cd9f432
heskyttberg <heskyttberg@gmail.com>
2024-05-27 20:15:55 +0200

    Finished JS exercise 7 both cmd and web

--- exercises/Week/1/JS/Exercise07.js
+++ exercises/Week/1/JS/Exercise07.js
@@ -1,15 +1,17 @@
-const num = parseInt(document.getElementById("num").value);
-
-let msg = '';
-
-if (num < 0)
-    msg = "not positive number";
-else if (cmp % 2 == 0)
-    msg = "Even number";
-else 
-    msg = "Odd number";
 
 function Display(){
-    document.getElementById("show").innerHTML = num;
-    document.getElementById("msg").innerHTML = msg;
+    let num1 = document.getElementById("num1");
+
+    let num1Value = parseInt(num1.value);
+
+    let msg='';
+
+    if(num1Value<0)
+        msg = "Not positive number"
+    else if(num1Value%2==0)
+        msg = "Even number";
+    else
+        msg = "Odd Number";
+
+    document.getElementById("show").innerHTML = msg;
 }
