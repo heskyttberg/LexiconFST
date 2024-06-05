2e0703c3b5eec44dc9a8692128ab97a5131f903f
heskyttberg <heskyttberg@gmail.com>
2024-06-05 10:15:10 +0200

    scrabbleScore done!

--- exercises/Week/3/TS/js-basics-assignment/src/index.js
+++ exercises/Week/3/TS/js-basics-assignment/src/index.js
@@ -370,6 +370,7 @@ function fibonacci(fNum) {
     else
         return fibonacci(fNum - 1) + fibonacci(fNum - 2);
 }
+
 function runExercise17() {
     var prompt = require("prompt-sync")();
     var fLen = 0;
@@ -379,6 +380,71 @@ function runExercise17() {
     for (var i = 0; i < fLen; i++)
         console.log(fibonacci(i));
 }
+
+// #################################### scrabbleWordScore #################
+
+// To get the value for a single letter
+function scoreLetter(c: any): number{
+    let scoreTable = {
+        A : 1,
+        E : 1,
+        I : 1,
+        O : 1,
+        U : 1,
+        L : 1,
+        N : 1,
+        R : 1,
+        S : 1,
+        T : 1,
+        D : 2, 
+        G : 2,
+        B : 3,
+        C : 3,
+        M : 3,
+        P : 3,
+        F : 4,
+        H : 4,
+        V : 4,
+        W : 4,
+        Y : 4,
+        K : 5,
+        J : 6,
+        X : 6,
+        Q : 7, 
+        Z : 7
+    };
+
+    // We need to ignore this line because getting this correct is almost impossible and too much work
+    // very complicated to get TS to accept this statement, key, value arrays is basically useless in TS
+    // @ts-ignore
+    return scoreTable[c];
+}
+
+// Looping through our word and sum up total score
+function scoreWord (str: string){
+
+    let sum = 0;
+    let i = 0;
+
+    for(i=0;i<str.length; i++)
+        sum += scoreLetter(str[i]);
+
+    return sum;
+}
+
+//let str = "cabbage";
+//let msg = scoreWord(str.toUpperCase());
+function scrabbleScore(){
+    let str = "";
+    str = readLine("Write your word: ");
+
+    let score = scoreWord(str);
+    
+    let msg = "Your word \"" + str + "\" is worth " + score + " points."
+    console.log(msg);
+}
+
+// ########################################################################
 /* ^^^^^^^^^^^^  Add the rest of the exercise functions above this line ^^^^^^^^^^^^ */
 var keepAlive = true;
 console.clear();
@@ -437,6 +503,9 @@ while (keepAlive) {
                 break;
             case 17:
                 runExercise17();
+                break;
+            case 100:
+                scrabbleScore();
                 break;
             /* ^^^^^^^^^^^^  Add the rest of the exercises above this line ^^^^^^^^^^^^ */
             case -1:
--- exercises/Week/3/TS/js-basics-assignment/src/index.ts
+++ exercises/Week/3/TS/js-basics-assignment/src/index.ts
@@ -466,6 +466,71 @@ function runExercise17() {
     console.log(fibonacci(i));
 }
 
+// #################################### scrabbleWordScore #################
+
+// To get the value for a single letter
+function scoreLetter(c: any): number{
+  let scoreTable = {
+      A : 1,
+      E : 1,
+      I : 1,
+      O : 1,
+      U : 1,
+      L : 1,
+      N : 1,
+      R : 1,
+      S : 1,
+      T : 1,
+      D : 2, 
+      G : 2,
+      B : 3,
+      C : 3,
+      M : 3,
+      P : 3,
+      F : 4,
+      H : 4,
+      V : 4,
+      W : 4,
+      Y : 4,
+      K : 5,
+      J : 6,
+      X : 6,
+      Q : 7, 
+      Z : 7
+  };
+
+  // We need to ignore this line because getting this correct is almost impossible and too much work
+  // very complicated to get TS to accept this statement, key, value arrays is basically useless in TS
+  // @ts-ignore
+  return scoreTable[c];
+}
+
+// Looping through our word and sum up total score
+function scoreWord (str: string){
+
+  let sum = 0;
+  let i = 0;
+
+  for(i=0;i<str.length; i++)
+      sum += scoreLetter(str[i]);
+
+  return sum;
+}
+
+//let str = "cabbage";
+//let msg = scoreWord(str.toUpperCase());
+function scrabbleScore(){
+  let str = "";
+  str = readLine("Write your word: ");
+  let score = 0;
+
+  score = scoreWord(str.toUpperCase());
+
+  let msg = "Your word \"" + str + "\" is worth " + score + " points."
+  console.log(msg);
+}
+
+// ########################################################################
 
 /* ^^^^^^^^^^^^  Add the rest of the exercise functions above this line ^^^^^^^^^^^^ */
 
@@ -474,7 +539,7 @@ console.clear();
 while (keepAlive) {
   try {
     const assignmentChoice = parseInt(
-      readLine("Enter assignment number (ctrl + C or -1 to exit): ")
+      readLine("Enter assignment number [1-17, 100] (ctrl + C or -1 to exit): ")
     );
     console.log();
     switch (assignmentChoice) {
@@ -528,6 +593,9 @@ while (keepAlive) {
         break;
       case 17:
         runExercise17();
+        break;
+      case 100:
+        scrabbleScore();
         break;
 
       /* ^^^^^^^^^^^^  Add the rest of the exercises above this line ^^^^^^^^^^^^ */
